57c896f3c638131d74ca5ac87429b9749080cab9
Victoria Bright <victoriacbright92@gmail.com>
2022-09-28 09:04:12 -0500

    completed

---
+++ script.js
@@ -0,0 +1,33 @@
+// const
+
+let num = 20; 
+
+num = 15; console.log(num);
+
+num = num +7; console.log(num);
+
+num --; console.log(num);
+
+num = "This is not a number"; console.log(num);
+
+let blank; 
+
+console.log(blank);
+
+blank = null; console.log(blank);
+
+//BONUS
+
+//1
+const quarter=0.25, dime=0.10, nickel=0.05, penny=0.01;
+//const [quarter, dime, nickel, penny] = [0.25, 0.10, 0.05, 0.01];
+
+//2
+console.log(quarter, dime, nickel, penny);
+
+//3
+let letters = "qwertyuiop";
+
+//4
+console.log(letters [9])
+//console.log(letters[letters.length -1])
